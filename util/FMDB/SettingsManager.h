1d7089fa71ab1f447f63408fdb85b1cbe251e269
liao3841054 <251180323@qq.com>
2013-06-09 15:12:18 +0800

    ThailandForFun
    just do it  for testing all kinds of coding pieces,the project's
    subject is Thailand trip plan,hoping my pre-girlfriend who will having
    the trip with me  will like it.

---
+++ util/FMDB/SettingsManager.h
@@ -0,0 +1,19 @@
+//
+//  SettingsManager.h
+//  MobileKPI
+//
+//  Created by Wang Xiaolong on 12-9-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SettingsManager : NSObject
+{
+    int backwardNum;
+    Boolean isChange;
+}
+
+@property(nonatomic)int backwardNum; 
+
+@end
---
+++ util/PathHelper.h
@@ -0,0 +1,16 @@
+//
+//  PathHelper.h
+//  StrategicMap
+//
+//  Created by WanC on 13-5-20.
+//  Copyright (c) 2013年 wxl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PathHelper : NSObject
+
++(NSString*)getSandBoxPath;
++(NSString*)getLoacalImagesPath;
+
+@end
